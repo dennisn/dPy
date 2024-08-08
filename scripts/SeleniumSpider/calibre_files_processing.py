9a5caa969aa6b16211f29aa40058419ba409c3a0
dennisn <dennis.nguyen.06@gmail.com>
2024-08-08 19:22:25 +1000

    Update git ignore and Caliber processing

--- scripts/SeleniumSpider/calibre_files_processing.py
+++ scripts/SeleniumSpider/calibre_files_processing.py
@@ -30,6 +30,9 @@ def main():
         r'<p style="margin-bottom: 25px;"><a style="font-size: 26px; color: #000; " href="https://www.facebook.com/123Truyen" target="_blank">Click Theo Dõi -&gt; <span style="color: red;">Fanpage</span> Để Cập Nhật Truyện Cẩu Tại Yêu Võ Loạn Thế Tu Tiên</a></p>',
         r'<p style="font-size: 0.8em; font-style: italic;">Thông Báo: Website chuyển qua sử dụng tên miền mới <a href="https://123truyennn.com/">123truyennn.com</a> , Chúc bạn đọc truyện vui vẻ!</p>',
         r'<p></p><br>Truyện việt, cẩu đạo, top1 nhân khí cách top2 gần 20% số điểm! <p></p>',
+        r'<p style="margin-bottom: 25px;"><a style="font-size: 26px; color: #000; " href="https://www.facebook.com/123Truyen" target="_blank">Click Theo Dõi -&gt; <span style="color: red;">Fanpage</span> Để Cập Nhật Truyện Tu Tiên: Cẩu Tại Dược Viên Làm Ruộng Cầu Trường Sinh</a></p>',
+        r'<p style="font-size: 0.8em; font-style: italic;">Thông Báo: Website chuyển qua sử dụng tên miền mới <a href="https://123truyenii.com/">123truyenii.com</a> , Chúc bạn đọc truyện vui vẻ!</p>',
+        r'<p></p><br>Độc Y, có thể hồi máu, có thể kèm độc trong kỹ năng, võng du giải trí cực tốt, truyện đã full, bạo chương nhanh <p></p>                    <p style="margin-bottom: 25px;"><a style="font-size: 35px; color: #000; " href="https://www.facebook.com/123Truyen" target="_blank">Click Theo Dõi -&gt; <span style="color: red;">Fanpage</span> để cập nhật website</a></p>',
     ]
     
     base_dir = r"D:\Temp\VanSaoCong_CauTaiYeuVoLoanTheTuTien"
